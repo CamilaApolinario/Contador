15f8d4d524154a7953d5c7c2e080df534238a7a4
Camila Apolinario <Ka_millapolinario@hotmail.com>
2022-09-12 12:07:42 -0300

    Adicionar arquivos de projeto.

---
+++ Contador/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Contador
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número!");
+            _ = int.TryParse(Console.ReadLine(), out var resposta);
+
+            int soma = 0;
+            for(int numero = 1; numero <= resposta; numero++)
+            {
+                if(numero % 3 == 0 || numero % 5 == 0) 
+                    soma += numero;               
+            }
+
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+    }
+}
